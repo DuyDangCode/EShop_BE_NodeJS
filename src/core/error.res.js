78e4ec9b9b616d40118a4550914e5e7653ca37de
DuyDangCode <21520774@gm.uit.edu.vn>
2024-03-22 10:08:09 +0700

    hadle shutdown server

--- src/core/error.res.js
+++ src/core/error.res.js
@@ -1,16 +1,17 @@
-import { statusCodes } from './httpStatusCode/statusCodes.js';
-import { reasonPharses } from './httpStatusCode/reasonPhrases.js';
-import { message } from './httpStatusCode/message.js';
+import { statusCodes } from './httpStatusCode/statusCodes.js'
+import { reasonPharses } from './httpStatusCode/reasonPhrases.js'
+import { message } from './httpStatusCode/message.js'
 class ErrorRes extends Error {
   constructor(status, message) {
-    super(message);
-    this.status = status;
+    super(message)
+    this.status = status
+    // Error.captureStackTrace(this)
   }
 }
 
 class ConfigRequestError extends ErrorRes {
   constructor(status = statusCodes.CONFLICT, message = reasonPharses.CONFLICT) {
-    super(status, message);
+    super(status, message)
   }
 }
 
@@ -19,7 +20,7 @@ class BadRequestError extends ErrorRes {
     status = statusCodes.BAD_REQUEST,
     message = statusCodes.BAD_REQUEST
   ) {
-    super(status, message);
+    super(status, message)
   }
 }
 
@@ -28,7 +29,7 @@ class ForbiddenError extends ErrorRes {
     status = statusCodes.FORBIDDEN,
     message = reasonPharses.FORBIDDEN
   ) {
-    super(status, message);
+    super(status, message)
   }
 }
 
@@ -37,16 +38,16 @@ class AuthFailError extends ErrorRes {
     status = statusCodes.BAD_REQUEST,
     message = reasonPharses.BAD_REQUEST
   ) {
-    super(status, message);
+    super(status, message)
   }
 }
 
 class VoucherInvalidError extends ErrorRes {
   constructor({
     status = statusCodes.BAD_REQUEST,
-    message = message.VoucherInvalid,
+    message = message.VoucherInvalid
   }) {
-    super(status, message);
+    super(status, message)
   }
 }
 
@@ -55,5 +56,5 @@ export {
   BadRequestError,
   ForbiddenError,
   AuthFailError,
-  VoucherInvalidError,
-};
+  VoucherInvalidError
+}
--- src/helpers/index.helper.js
+++ src/helpers/index.helper.js
@@ -4,4 +4,25 @@ const asyncHandler = (fn) => {
   }
 }
 
-export { asyncHandler }
+const terminate = (
+  server,
+  options = {
+    coredump: false,
+    timeout: 500
+  }
+) => {
+  const exit = (code) => {
+    options.coredump ? process.abort() : process.exit(code)
+  }
+
+  return (code, reason) => (err, promise) => {
+    console.log(reason)
+    if (err && err instanceof Error) {
+      console.log(err.message, err.stack)
+    }
+    server.close(exit)
+    setTimeout(exit, options.timeout).unref()
+  }
+}
+
+export { asyncHandler, terminate }
