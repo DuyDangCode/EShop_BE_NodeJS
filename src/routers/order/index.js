2dff036a46db6a2952a9dad92a660ea1bcc1f645
Thành Duy <21520774@gm.uit.edu.vn>
2024-04-11 08:25:33 +0700

    add authentication

--- src/routers/order/index.js
+++ src/routers/order/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import OrderController from '../../controllers/order.controller.js'
 import { asyncHandler } from '../../helpers/index.helper.js'
+import { authentication } from '../../auth/checkAuth.js'
 
 const orderRouter = express.Router()
 
+orderRouter.use(authentication)
 orderRouter.post('/review', asyncHandler(OrderController.checkout))
 orderRouter.post('', asyncHandler(OrderController.order))
 
